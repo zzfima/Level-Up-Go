019a130c511a2d59bd8ebf11d60adce6dd9438b0
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-01-03 16:01:40 +0200

    add channel for sync

--- concurrencyChallenges/stopCopyingMe.go
+++ concurrencyChallenges/stopCopyingMe.go
@@ -26,8 +26,8 @@ func repeatRecursive(n int, message string) {
 
 var wg sync.WaitGroup
 
-// repeat concurrently prints out the given message n times
-func repeat(n int, message string) {
+// repeat concurrently using WaitGroup prints out the given message n times
+func repeatWaitGroup(n int, message string) {
 	for i := 0; i < n; i++ {
 		log.Println(message)
 		wg.Done()
@@ -36,13 +36,34 @@ func repeat(n int, message string) {
 
 // 1 rename to main
 // 2 run in terminal: go run .\stopCopyingMe.go -factor 2
-func mainStopCopyingMe() {
+func mainStopCopyingMeWaitGroup() {
 	factor := flag.Int64("factor", 0, "The fan-out factor to repeat by")
 	flag.Parse()
 	n := int(*factor)
 	for _, m := range messages {
 		wg.Add(n)
-		go repeat(n, m)
+		go repeatWaitGroup(n, m)
 	}
 	wg.Wait()
+}
+
+// repeat concurrently using Channel prints out the given message n times
+func repeatChannel(n int, message string, ch chan struct{}) {
+	for i := 0; i < n; i++ {
+		log.Println(message)
+	}
+	ch <- struct{}{}
+}
+
+// 1 rename to main
+// 2 run in terminal: go run .\stopCopyingMe.go -factor 2
+func mainStopCopyingMeChannel() {
+	factor := flag.Int64("factor", 0, "The fan-out factor to repeat by")
+	flag.Parse()
+	n := int(*factor)
+	ch := make(chan struct{})
+	for _, m := range messages {
+		go repeatChannel(n, m, ch)
+		<-ch
+	}
 }
